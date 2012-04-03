a8450aa085a71e109ecf29d65253f2320f09a688
Eugeniy Khutornoy <evgenij.xy@gmail.com>
2012-04-03 22:33:35 +0400

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diplom::Application.config.secret_token = 'becdbede593f5bf3f3ddc0edceb3cffe0c5f36e00a7dbf053ea9127c06b6955db06d4ab960ee68f672ca180468d84fce22ff5c1edd46dc712d4bb45f72ac5710'
