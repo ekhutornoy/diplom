b489e74cce43ac4b2914536a1b5e7794c0b51eb1
Eugeniy Khutornoy <evgenij.xy@gmail.com>
2012-04-17 23:23:12 +0400

    Категории товаров

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :name, :num
 
   has_many :stocks, :dependent => :destroy
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120414090031) do
+ActiveRecord::Schema.define(:version => 20120417191959) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "departments", :force => true do |t|
     t.string   "name"
@@ -47,6 +53,7 @@ ActiveRecord::Schema.define(:version => 20120414090031) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
   create_table "stocks", :force => true do |t|
