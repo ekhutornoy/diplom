4731bc142adee474980a0e31882dc8420dac6bd7
Eugeniy Khutornoy <evgenij.xy@gmail.com>
2012-04-24 10:28:02 +0400

    fix: дерево категорий в остатках

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,11 @@ class StocksController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @stocks = current_company.stocks.page(params[:page])
+    @stocks = current_company.stocks
+    if params[:category_id].present?
+      @stocks = @stocks.joins(:product).
+        where('products.category_id' => params[:category_id])
+    end
+    @stocks = @stocks.page(params[:page])
   end
 end
