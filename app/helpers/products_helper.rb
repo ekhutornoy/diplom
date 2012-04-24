c484f4e4fa00de9f1e29490524923766d2348d0b
Eugeniy Khutornoy <evgenij.xy@gmail.com>
2012-04-24 10:21:19 +0400

    Дерево категорий в остатках и товарах

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,8 @@
 module ProductsHelper
+  def products_category_tree(categories)
+    categories.collect do |c, sub|
+      link_to(c.name, products_path(:category_id => c.id)) +
+        content_tag(:div, products_category_tree(sub), :class => 'nested_categories')
+    end.join.html_safe
+  end
 end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,8 @@
 module StocksHelper
+  def stocks_category_tree(categories)
+    categories.collect do |c, sub|
+      link_to(c.name, stocks_path(:category_id => c.id)) +
+        content_tag(:div, stocks_category_tree(sub), :class => 'nested_categories')
+    end.join.html_safe
+  end
 end
