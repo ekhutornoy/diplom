924222d8f843a0aeabf5b7a4e022246172d1a402
Eugeniy Khutornoy <evgenij.xy@gmail.com>
2012-04-27 09:32:09 +0400

    Комментарии к правам доступа

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,39 +6,56 @@ class Ability
     user ||= User.new
 
     if user.admin?
-      # Если пользователь-админ, ему можно всё
+      # Если пользователь-админ, ему можно выполнять любые действия
+      # с ресурсамии компании
       can :manage, :all
     else
       # Просматривать остатки, приходы, продажи и товары могут все
       can :read, [Stock, Income, SaleOrder, Product]
 
+
       if user.has_permission?(:incomes)
+        # Пользователь с правов :imcomes может создавать приходы
         can :create, Income
+        # и выполнять любые действия со своими приходами
         can :manage, Income, :user_id => user.id
       end
 
       if user.has_permission?(:company_incomes)
+        # Пользователь с правом company_incomes может выполнять
+        # любые действия со всеми приходами компании
         can :manage, Income
       end
 
       if user.has_permission?(:sale_orders)
+        # Пользователь с правом :sale_orders может создавать продажи
         can :create, SaleOrder
+        # и выполнять любые действия со своими продажами
         can :manage, SaleOrder, :user_id => user.id
       end
 
       if user.has_permission?(:company_sale_orders)
+        # Пользователь с правом company_sale_orders может выполнять
+        # любые действия со всеми продажами компании
         can :manage, SaleOrder
       end
 
       if user.has_permission?(:products)
+        # Пользователь с правом products может выполнять
+        # любые действия со справочниками товаров и категорий
         can :manage, Product
+        can :manage, Category
       end
 
       if user.has_permission?(:departments)
+        # Пользователь с правом departments может выполнять
+        # любые действия со справочником складов
         can :manage, Department
       end
 
       if user.has_permission?(:company_users)
+        # Пользователь с правом company_users может выполнять
+        # любые действия с сотрудниками компании
         can :manage, User
       end
     end
